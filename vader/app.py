e968d6a939adec11a75953742f56badcfbc3ab2e
Franksalo <franco.salo@gmail.com>
2016-12-18 22:15:14 -0300

    Added gevent streaming

--- vader/app.py
+++ vader/app.py
@@ -4,7 +4,12 @@ import threading
 from main import *
 from logger import  create_rotating_log
 from camera import Camera
+import base64
+import gevent
+import gevent.monkey
+from gevent.pywsgi import WSGIServer
 
+gevent.monkey.patch_all()
 application = Flask(__name__)
 
 @application.route('/')
@@ -14,12 +19,9 @@ def index():
 
 
 def gen():
-    cameras = config_everything_and_get_cameras("config/config.yml", [1,2], test=False)
-    for camera in cameras:
-        camera.configure_streaming()
-    frame_list = [camera.get_frame() for camera in cameras]  # Fills an array with "None" values
     LOGGER.info("Starting the program. Cameras count: %s" % str(len(cameras)))
     while True:
+        gevent.sleep(0.001)
         for index, camera in enumerate(cameras):
             camera.single_capture(test=False)
             if camera.get_frame() is not None:
@@ -28,15 +30,19 @@ def gen():
                 frame_list[index] = cv2.imread('img/loading.jpg')
         cv2.imwrite('t.jpg', get_image_stack(frame_list))
         cv2.waitKey(15)
-        yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+        yield ('data: ' + base64.b64encode(open('t.jpg', 'rb').read()) + '\n\n')
 
 
 @application.route('/video_feed')
 def video_feed():
     return Response(gen(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+                    mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    application.logger.addHandler(create_rotating_log('/var/log/count.log', 'info'))
-    application.run(host='0.0.0.0', threaded=True)
+    global cameras
+    cameras = config_everything_and_get_cameras("config/config.yml", [1,2], test=False)
+    for camera in cameras:
+        camera.configure_streaming()
+    frame_list = [camera.get_frame() for camera in cameras]  # Fills an array with "None" values
+    http_server = WSGIServer(('127.0.0.1', 8001), application.wsgi_app)
+    http_server.serve_forever()
